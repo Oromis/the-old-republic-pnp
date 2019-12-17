f19e02dd41c63ecafdbd90d3c531ea0559ea4412
Silas Bartel <silas.a.bartel@gmail.com>
2019-12-17 22:58:47 +0100

    Fixed training effect calculation. Added species to actor template.

--- module/CharacterFormulas.js
+++ module/CharacterFormulas.js
@@ -76,18 +76,18 @@ export function calcSkillXp(actor, property) {
 function calcXpFromTrainingSkill(training, property, from) {
   const key = getKey(property)
   if(training.data.baseTraining) {
-    from += (+training.data.baseTraining.data.effects[key]) || 0
+    from += (+training.data.baseTraining.data.effects.filter(e => e.key === property.key).reduce((acc, cur) => acc + (+cur.value), 0))
   }
   return XpTable.getUpgradeCost({
     category: property.xpCategory,
     from: from,
-    to: from + (+training.data.effects[key])
+    to: from + (+training.data.effects.filter(e => e.key === property.key).reduce((acc, cur) => acc + (+cur.value), 0))
   })
 }
 
 export function calcPropertyTrainingsEffects(actor, property) {
   if(detectPropertyType(property) !== 'skill') {
-    return actor.trainings.map(t => ObjectUtils.try(t.data.effects, getKey(property), {default: 0})).reduce((acc, v) => acc + (+v), 0)
+    return actor.trainings.map(t => t.data.effects.filter(e => e.key === property.key).reduce((acc, cur) => acc + (+cur.value), 0)).reduce((acc, v) => acc + (+v), 0)
   }
   const from = property.isBasicSkill ? 5 : 0
   const xp = actor.trainings.map(t => calcXpFromTrainingSkill(t, property, from)).reduce((acc, v) => acc + (+v), 0)
