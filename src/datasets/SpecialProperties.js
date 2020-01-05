41a61b3051c281e30d992bca9681f7940d31b4f7
David <david.bauske@googlemail.com>
2020-01-05 14:49:41 +0100

    Added energy generator power special property

--- src/datasets/SpecialProperties.js
+++ src/datasets/SpecialProperties.js
@@ -3,6 +3,7 @@ import ObjectUtils from '../util/ObjectUtils.js'
 const list = [
   { key: 'InI', label: 'Initiative' },
   { key: 'LaW', label: 'Laufweg' },
+  { key: 'EgL', label: 'Energiegenerator-Leistung' },
 ]
 
 export default Object.freeze({
