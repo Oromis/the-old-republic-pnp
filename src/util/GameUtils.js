7beba7c82eb12fd77394bc53249e2d6475aceb0b
David <oromis@elefantracing.de>
2020-01-09 18:59:46 +0100

    Fixed turn regeneration for token actors

--- src/util/GameUtils.js
+++ src/util/GameUtils.js
@@ -1,8 +1,21 @@
 export function getActorByTokenId(tokenId, { scene = game.scenes.active } = {}) {
-  const token = scene.getEmbeddedEntity('tokens', tokenId)
-  if (token != null) {
-    return game.actors.get(token.actorId)
+  if (canvas.scene === scene) {
+    // Scene is currently active => query the token from the TokenLayer. This works for TokenActors
+    // and regular Actors alike.
+    const token = canvas.tokens.get(tokenId)
+    if (token != null) {
+      return token.actor
+    } else {
+      return null
+    }
   } else {
-    return null
+    // Scene is not currently active. The following will only work for linked actors
+    // (which is not a big problem in practice)
+    const token = scene.getEmbeddedEntity('tokens', tokenId)
+    if (token != null) {
+      return game.actors.get(token.actorId)
+    } else {
+      return null
+    }
   }
 }
